02b0b442f4af025e74ae0e8a4c0f1bbc6a392e7b
SUJIT MANDAL <mandals974@gmail.com>
2019-04-14 17:07:15 +0530

    first commit

---
+++ BobbleSort/src/Bubble_Sort_For_Decreasing_Order.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Bubble_Sort_For_Decreasing_Order {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int  i, j, swap;
+	    Scanner input = new Scanner(System.in);
+	 
+	    System.out.println("Enter number of elements:");
+	    int n = input.nextInt();
+	 
+	    int data[] = new int[n];
+		 
+	    System.out.println("Enter " + n + " integers:");
+	 
+	    for (i = 0; i < n; i++)
+	   	 data[i]  = input.nextInt();
+	   
+	    for (i = 0; i < ( n - 1 ); i++) {
+	      for (j = 0; j < n - i - 1; j++) {
+	        if (data[j] < data[j+1]) /* For ascending order use > */
+	        {
+	          swap       = data[j];
+	          data[j]   = data[j+1];
+	          data[j+1] = swap;
+	        }
+	      }
+	    }
+	 
+	    System.out.println("\nSorted list of numbers:");
+	 
+	    for (i = 0; i < n; i++)
+	      System.out.println(data[i]);
+
+	}
+
+}
