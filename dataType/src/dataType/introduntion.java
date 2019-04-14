b2dcff2a4a37c8c9713619edfa6a799756236289
SUJIT MANDAL <mandals974@gmail.com>
2019-04-14 15:18:16 +0530

    first commit

---
+++ dataType/src/dataType/introduntion.java
@@ -0,0 +1,31 @@
+/* java data type */
+package introduction;
+/*
+This program created by Sujit Mandal 
+*/
+
+public class introduction {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int     a= 27;
+		float   b= 25.25f;
+		double  c= 258.288;
+		char    d= 's';
+		long    e= 25857;
+		short   f= 2158;
+		boolean g= true;
+		byte    h= 127;
+		
+		int r = 127;
+		
+		System.out.println("a= " + a);
+		System.out.println("b= " + b);
+		System.out.println("c= " + c);
+		System.out.println("d= " + d);
+		System.out.println("e= " + e);
+		System.out.println("f= " + f);
+		System.out.println("g= " + g);
+		System.out.println("h= " + h);
+		System.out.println("r= " + r);
+	}
+}
